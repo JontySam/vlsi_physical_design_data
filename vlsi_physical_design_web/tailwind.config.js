49af85922b78a3b04bb3ef39ccc5c2ee5f0436b2
JontySam <jontysam15@gmail.com>
2025-09-19 19:01:55 -0500

    seo optimizations

---
+++ vlsi_physical_design_web/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Scan all HTML files in this directory for Tailwind classes
+    './*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
